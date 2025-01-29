83bbb6ecdab2bc3f8a6e1c83858f23e435f8256f
snou <dablosk@proton.me>
2025-01-29 19:10:03 +0000

    Final 29/1/24

---
+++ Python/01_Practica.py
@@ -0,0 +1,8 @@
+# nombre apellido edad y luego una frase
+name = "Dani"
+last = "Arribas"
+age = 38
+frase = "Me llamo"
+frase_1 = "tengo"
+
+print(frase,name,last,frase_1,age)
--- Python/01_caracteristicas_basicas.py
+++ Python/01_caracteristicas_basicas.py
@@ -18,11 +18,31 @@ de estos errores nos avisa VSC
 No de debemos hacer (no son exactamente errores):
     contener caracteres fuera del idioma ingles, como ñ, ç, tildes, Etc
     Empezar por guion bajo (reservado para determinadas situaciones)
+    No debemos utilizar palabras utilizadas por el sistema  
 
 Que debemos hacer:
     Nombra a nuestras variables con palabras descriptivas
     Podemos usar mas de una palabra separadas por un guion bajo (Directiva PEP-8)
     Intentar que las lineas de codigo no sean mu largas
 
+Las variables tienen tipos:
+    -- numeros => enteros (int), decimales (float), complejos ()
+    -- texto => strings
+    -- booleanos => True / False
+
+Python es de tipado dinamico
+
+numero = 1 # entero
+numero = "uno" # string
+
+Python es de tipado fuerte 
+
+suma = numero + 2 # Error > no se pueden sumar numeros y texto
+concatenacion = numero + str(2)
+suma_numerica = int("1") + 2
+
+En python, por defecto NO exiten las constante
+
+PI = 3.1416
 
 """
