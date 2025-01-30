6d561a95b81d96c092f5f5189831b161df4399c9
snou <dablosk@proton.me>
2025-01-30 17:47:40 +0000

    18:41

--- Python/Practicas/03_practica.py
+++ Python/Practicas/03_practica.py
@@ -4,10 +4,10 @@
 # si tienes menos de 30 años eres joven
 # si tienes menos de 40 años eres adulto
 # si tienes mas de 40 tu puedes con todo
-
+"""
 edad = int(input("Pon tu edad ->"))
 
-if 1 < edad < 12:
+if 0 <= edad < 12:
     print("Eres un niño/a")
 elif edad <= 18:
     print("Eres adolescente")
@@ -15,5 +15,25 @@ elif edad <= 30:
     print("Eres Joven")
 elif edad <= 40:
     print("Eres adulto")
-else:
+elif edad <= 120:
     print("Tu puedes con todo")
+else:
+    print("Tienes 2 pies en la tumba")
+"""
+# Preguntar al usuario la edad
+# si tiene 0 o mas de 120: No me lo creo
+# si tiene menos de 18: Aun no puedes votar, te faltan x años
+# si tienes mas de 18: Puedes votar desde hace x años
+
+edad = int(input("Pon tu edad ->"))
+
+if 0 > edad:
+    print('NO me lo creo')
+elif edad < 18:
+    print("Aun no puedes votar, te faltan " + str(18 - edad) + " años")
+elif edad >= 18:
+    print("Puedes votar desde hace " + str(edad - 18) + " años")
+elif edad >= 120:
+    print("No me lo creo")
+else:
+    pass
