c9e4995aef755a49577cd0a0c14605a300cd2af7
snou <dablosk@proton.me>
2025-02-03 09:11:59 +0100

    revision

--- Python/Practicas/03_2practica.py
+++ Python/Practicas/03_2practica.py
@@ -1,25 +1,25 @@
 # Solucion 3 calculadora Ferran
 import os
 os.system("cls")
-"""
+
 try:
-    respuesta = input("Inndica los numero y la operacion a realizar:\nEjemplo: 10, 5, +\n\n").split(", ")
-    num_1 = float(respuesta[0])
-    num_2 = float(respuesta[1])
-    op = respuesta[2]
+    respuesta = input("Inndica los numero y la operacion a realizar:\nEjemplo: 10, 5, +\n\n").split(", ") # Separa la entrada en numeros y operacion
+    num_1 = float(respuesta[0]) # Convierte los datos proporcionados por el usuario a float y los guarda en la variable num_1
+    num_2 = float(respuesta[1]) # Convierte los datos proporcionados por el usuario a float y los guarda en la variable num_2 
+    op = respuesta[2] # Guarda la operacion en una variable
 
     match op:
-        case "+" | "-" | "*" | "**" | "/" | "//" | "%":
-            resultado = eval(f"{num_1} {op} {num_2}")
-            print(f"{num_1} {op} {num_2} = {resultado}")  
-        case _:
-            print("Operacion desconocida")
+        case "+" | "-" | "*" | "**" | "/" | "//" | "%": # Comprueba las operaciones disponibles con la variable op
+            resultado = eval(f"{num_1} {op} {num_2}") # Evalúa la operación y guarda el resultado en la variable resultado
+            print(f"{num_1} {op} {num_2} = {resultado}")  # Muestra el resultado de la operación
+        case _: # Si la operacion no es conocida, muestra el mensaje 
+            print("Operacion desconocida") 
             
-except ValueError:
+except ValueError: # Si la entrada no es un numero, muestra el mensaje 
     print("Introduce un numero en Cifras")
-except ZeroDivisionError:
+except ZeroDivisionError: # Si se intenta dividir entre cero, muestra el mensaje 
     print("No se puede dividir por Cero")
-"""
+
 # Ejecicio Palindrono
 # Solicitat al usuario un texto y le vas a decir si es un palindromo o no
 
