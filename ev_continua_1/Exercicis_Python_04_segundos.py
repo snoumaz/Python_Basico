150b246ed63e1870630c9dba3409568cf1d27c2d
snoumaz <dablosk@proton.me>
2025-02-05 22:06:32 +0100

    0

--- ev_continua_1/Exercicis_Python_04_segundos.py
+++ ev_continua_1/Exercicis_Python_04_segundos.py
@@ -19,27 +19,3 @@ Lo mismo si supera un día o una semana.
 . 
 """
 
-segundos_ingresados = int(input("Conversor de segundos\nIngrese la cantidad de segundos: "))
-
-semanas = segundos_ingresados // 604800
-segundos_ingresados %= 604800
-dias = segundos_ingresados // 86400
-segundos_ingresados %= 86400
-horas = segundos_ingresados // 3600
-segundos_ingresados %= 3600
-minutos = segundos_ingresados // 60
-segundos_ingresados %= 60
-
-resultado = []
-if semanas > 0:
-    resultado.append(f"{semanas} semana{'s' if semanas > 1 else ''}")
-if dias > 0:
-    resultado.append(f"{dias} día{'s' if dias > 1 else ''}")
-if horas > 0:
-    resultado.append(f"{horas} hora{'s' if horas > 1 else ''}")
-if minutos > 0:
-    resultado.append(f"{minutos} minuto{'s' if minutos > 1 else ''}")
-if segundos_ingresados > 0 or not resultado:
-    resultado.append(f"{segundos_ingresados} segundo{'s' if segundos_ingresados > 1 else ''}")
-
-print(f"{segundos_ingresados} segundos son " + " y ".join(resultado) + ".")
