9b397e98dddb148550692b7150f9e9aebb282a8e
snoumaz <dablosk@proton.me>
2025-02-07 01:19:01 +0100

    comments

--- Python/Ejercicios_Logica/01_ejercicio.py
+++ Python/Ejercicios_Logica/01_ejercicio.py
@@ -19,14 +19,14 @@ try:
     os.system("cls")
     print("Lista Numeros")
     if 0<numero<=100: # Compara si la variable numero es mayor que 0 y menor de 100
-        for lista_numeros in range(numero + 1):
-            if lista_numeros == 0:
+        for lista_numeros in range(numero + 1): # Recorre la lista de numeros
+            if lista_numeros == 0: # Si el numero es 0 imprime 0
                 print(0)
-            elif lista_numeros % 3 == 0 and lista_numeros % 5 == 0:
+            elif lista_numeros % 3 == 0 and lista_numeros % 5 == 0: # Si el numero es multiplo de 3 y 5 imprime Fizz - Buzz
                 print(f"{lista_numeros} - Fizz - Buzz")
-            elif lista_numeros % 5 == 0:
+            elif lista_numeros % 5 == 0: # Si el numero es multiplo de 5 imprime Buzz
                 print(f"{lista_numeros} - Buzz")
-            elif lista_numeros % 3 == 0:
+            elif lista_numeros % 3 == 0: # Si el numero es multiplo de 3 imprime Fizz
                 print(f"{lista_numeros} - Fizz")
             else:
                 print(lista_numeros)
@@ -39,22 +39,22 @@ except ValueError:
 
 # Solucion Ferran
 
-try:
-    referencia = int(input("Introduce un numero del 1 al 100: \n"))
-    # if referencia < 0 or referencia > 100:
-    #     print("El numero es incorrecto")
-    #     exit()
-    if 0<= referencia <= 100:
-        for num in range(0, referencia+1, 1):
-            if num == 0:
-                print(0)
-            elif num % 3 == 0 and num % 5 == 0:
-                print(f"{num} -Fizz-Buzz")
-            elif num % 3 == 0:
-                print(f"{num} -Fizz")
-            elif num % 5 == 0:
-                print(f"{num} -Buzz")
-            else:
-                print(num)
-except ValueError:
-    print("El numero es incorrecto")
+# try:
+#     referencia = int(input("Introduce un numero del 1 al 100: \n"))
+#     # if referencia < 0 or referencia > 100:
+#     #     print("El numero es incorrecto")
+#     #     exit()
+#     if 0<= referencia <= 100: # Compara si la variable numero es mayor que 0 y menor de 100
+#         for num in range(0, referencia+1, 1):
+#             if num == 0:
+#                 print(0)
+#             elif num % 3 == 0 and num % 5 == 0:
+#                 print(f"{num} -Fizz-Buzz")
+#             elif num % 3 == 0:
+#                 print(f"{num} -Fizz")
+#             elif num % 5 == 0:
+#                 print(f"{num} -Buzz")
+#             else:
+#                 print(num)
+# except ValueError:
+#     print("El numero es incorrecto")
