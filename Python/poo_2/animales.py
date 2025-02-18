0263099050dcb12b4104a541c1ff6d854ab1238a
snoumaz <dablosk@proton.me>
2025-02-18 20:06:36 +0100

    18-02-25

--- Python/poo_2/animales.py
+++ Python/poo_2/animales.py
@@ -2,20 +2,26 @@
 Creacion clase
 """
 class Animal():
-    pass
+    def __init__(self,especie):
+        self.especie = especie
+    def __str__(self):
+        return f"La especie es {self.especie}"       
 
 class Perro(Animal):
     def sonido(self):
         print("GUAUUUUUU!!!")
 
-milu = Perro()
+tortuga = Animal("Tortuga")
+
+milu = Perro("Canido")
 milu.sonido()
+print(milu)
 
 class Gato(Animal):
     def sonido(self):
         print("MIAUUUUUU!!!")
 
-bola = Gato()
+bola = Gato("Felino")
 bola.sonido()
 
 print(Perro.__base__)
