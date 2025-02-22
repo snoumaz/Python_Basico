1685e36e2bb8c69ccf296f62ccb42a1bee2b9ca4
snou <dablosk@proton.me>
2025-02-22 16:13:17 +0100

    0

--- Python/Practicas/11_practica.py
+++ Python/Practicas/11_practica.py
@@ -17,39 +17,53 @@ La clase Moto tiene un método propio: revisar_seguridad, devuelve este mensaje:
 "Si circulas en motocicleta debes llevar casco"
 '''
 
-class Vehiculo():
-    def __init__(self,marca,modelo,año):
+# Definición de la clase base Vehiculo
+class Vehiculo(): 
+    # Constructor de la clase Vehiculo
+    def __init__(self, marca, modelo, año): 
         self.marca = marca
         self.modelo = modelo
         self.año = año
+    
+    # Método para arrancar el vehículo
     def arrancar(self):
         return f"El vehiculo {self.marca} modelo {self.modelo} ha arrancado"
+    
+    # Método para detener el vehículo
     def detener(self):
         return f"El vehiculo {self.marca} modelo {self.modelo} se ha detenido"
     
+# Definición de la clase Coche que hereda de Vehiculo
 class Coche(Vehiculo):
-    def __init__(self, marca, modelo, año,puertas,ac):
-        super().__init__(marca, modelo, año)
+    # Constructor de la clase Coche
+    def __init__(self, marca, modelo, año, puertas, ac):
+        super().__init__(marca, modelo, año)  # Llamada al constructor de la clase base
         self.puertas = puertas
         self.ac = ac
+    
+    # Método para abrir el maletero del coche
     def abrir_maletero(self):
-        if self.puertas >=1:
+        if self.puertas >= 1:
             return f"El maletero del {self.marca} {self.modelo} está abierto"
         
+# Definición de la clase Moto que hereda de Vehiculo
 class Moto(Vehiculo):
-    # def __init__(self, marca, modelo, año):
-    #     super().__init__(marca, modelo, año)
-
+    # Método para revisar la seguridad de la moto
     def revisar_seguridad(self):
         return f"Si circulas en motocicleta debes llevar casco"    
 
-coche_1 = Coche("Opel","Corsa",1992,3,"si")
-moto_1 = Moto("Yamaha","MT09","2025")
+# Creación de una instancia de la clase Coche
+coche_1 = Coche("Opel", "Corsa", 1992, 3, "si")
 
+# Creación de una instancia de la clase Moto
+moto_1 = Moto("Yamaha", "MT09", 2025)
+
+# Uso de los métodos de la instancia coche_1
 print(coche_1.arrancar())
 print(coche_1.detener())
 print(coche_1.abrir_maletero())   
 
+# Uso de los métodos de la instancia moto_1
 print(moto_1.arrancar())
 print(moto_1.detener())
 print(moto_1.revisar_seguridad())
